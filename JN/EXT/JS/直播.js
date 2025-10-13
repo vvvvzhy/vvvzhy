8434755cc86ee7504d8ae9156f5d4efde714998b
vvvvzhy <78501535@qq.com>
2025-10-13 23:04:18 +0800

    Delete JN directory
    i

--- JN/EXT/JS/直播.js
+++
@@ -1,8 +0,0 @@
-[
-{"name":"雷蒙影视概念宣传片","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/N3RD/master/JN/EXT/LIVE/ctvpv.txt"},
-{"name":"电视直播","url":"https://cors.isteed.cc/https://raw.githubusercontent.com/n3rddd/CTVLive/main/live.txt","group":1},
-{"name":"BiliBili直播","url":"https://www.goodiptv.club/bililive.m3u","group":1},
-{"name":"YY轮播","url":"https://www.goodiptv.club/yylunbo.m3u?url=https://lunbo.freetv.top","group":1},
-{"name":"斗鱼一起看","url":"https://live.freetv.top/douyuyqk.m3u","group":1},
-{"name":"虎牙一起看","url":"https://live.freetv.top/huyayqk.m3u","group":1}
-]
